d2c04aab4e0107c4175cfcc05764878a35725bf1
Aaron <Bowles@Aarons-MacBook-Pro.local>
2019-10-09 14:28:49 -0400

    for deployment

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose')
+
 mongoose.Promise = Promise
+
 let mongoURI = "";
 if (process.env.NODE_ENV === "production") {
   mongoURI = process.env.DB_URL;
 } else {
-  mongoURI = "mongodb://localhost/recipe";
+  mongoURI = "mongodb://localhost/recipes";
 }
-mongoose.connect('mongodb://localhost/recipe',{ useNewUrlParser: true,useUnifiedTopology: true,
+mongoose.connect('mongodb://localhost/recipes',{ useNewUrlParser: true,useUnifiedTopology: true,
 useFindAndModify: false}, () => {
     console.log("We connected!!!")
   })
