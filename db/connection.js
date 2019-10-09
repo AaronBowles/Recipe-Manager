170b1b25f72545a2d6ab796ebc1c76783247ad9c
Aaron <Bowles@Aarons-MacBook-Pro.local>
2019-10-09 14:16:08 -0400

    connection logic

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,13 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(mongoURI,{ useNewUrlParser: true}, () => {
-    console.log("We connected!!!")
-  })
-
-mongoose.Promise = Promise;
-
+const mongoose = require('mongoose')
+mongoose.Promise = Promise
 let mongoURI = "";
 if (process.env.NODE_ENV === "production") {
   mongoURI = process.env.DB_URL;
 } else {
   mongoURI = "mongodb://localhost/recipes";
 }
-
-module.exports = mongoose
+mongoose.connect('mongodb://localhost/recipes',{ useNewUrlParser: true,useUnifiedTopology: true,
+useFindAndModify: false}, () => {
+    console.log("We connected!!!")
+  })
+  module.exports = mongoose
